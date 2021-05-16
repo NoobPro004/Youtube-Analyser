07790b49a16346181861d7760ce3469cf59cf7c2
Him_garg <himanshugarg738@gmail.com>
2021-05-16 14:27:36 +0530

    Complete

--- yt.js
+++ yt.js
@@ -43,30 +43,24 @@ async function fn() {
     );
     let noOfVideos = obj.noOfVideos.split(" ")[0];
     noOfVideos = Number(noOfVideos);
-    // let i = 0;
-    // while (i <= noOfViews / 100) {
-    //   await scrollDown();
-    //   i++;
-    // }
-
-    //   videos_title -> #video-title
-    // duration-> span.style-scope.ytd-thumbnail-overlay-time-status-renderer
+    console.log(noOfVideos);
     let videoSelector = "#video-title";
     let duration =
       "span.style-scope.ytd-thumbnail-overlay-time-status-renderer";
-
-    await page.waitForSelector(
-      "span.style-scope.ytd-thumbnail-overlay-time-status-renderer",
-      { visible: true }
-    );
-    await page.waitForSelector("#video-title", { visible: true });
-    await page.evaluate(function () {
-      let allElements = document.querySelectorAll(
-        "#video-title"
-      );
-      allElements[allElements.length - 1].scrollIntoView(true);
-    });
-
+    let i = 0;
+    while (noOfVideos-cVideos>=100) {
+      await scrollDown();
+      i++;
+    }
+   
+   // await page.waitForNavigation({waitUntil:"networkidle0"});
+    //   videos_title -> #video-title
+    // duration-> span.style-scope.ytd-thumbnail-overlay-time-status-renderer
+    
+    await  waitTillHTMLRendered(page);
+    await scrollDown();
+    await  waitTillHTMLRendered(page);
+    console.log(cVideos);
     let titleDurArr = await getTitleNDuration(videoSelector, duration);
     console.table(titleDurArr);
   } catch (err) {
@@ -92,5 +86,51 @@ async function getTitleNDuration(videoSelector, duration) {
   );
   return titleDurArr;
 }
+async function scrollDown(){
+
+    await page.waitForSelector("span.style-scope.ytd-thumbnail-overlay-time-status-renderer", { visible: true });
+    await page.waitForSelector("#video-title", { visible: true });
+    cVideos=await page.evaluate(function () {
+      let allElements = document.querySelectorAll(
+        "#video-title"
+      );
+      allElements[allElements.length - 1].scrollIntoView(true);
+
+      return allElements.length;
+    });
+}
+
+async function waitTillHTMLRendered(page, timeout = 30000){
+    const checkDurationMsecs = 1000;
+    const maxChecks = timeout / checkDurationMsecs;
+    let lastHTMLSize = 0;
+    let checkCounts = 1;
+    let countStableSizeIterations = 0;
+    const minStableSizeIterations = 3;
+  
+    while(checkCounts++ <= maxChecks){
+      let html = await page.content();
+      let currentHTMLSize = html.length; 
+  
+      let bodyHTMLSize = await page.evaluate(() => document.body.innerHTML.length);
+  
+      console.log('last: ', lastHTMLSize, ' <> curr: ', currentHTMLSize, " body html size: ", bodyHTMLSize);
+  
+      if(lastHTMLSize != 0 && currentHTMLSize == lastHTMLSize) 
+        countStableSizeIterations++;
+      else 
+        countStableSizeIterations = 0; //reset the counter
+  
+      if(countStableSizeIterations >= minStableSizeIterations) {
+        console.log("Page rendered fully..");
+        break;
+      }
+  
+      lastHTMLSize = currentHTMLSize;
+      await page.waitFor(checkDurationMsecs);
+    }  
+  };
+
+
 
 fn();
